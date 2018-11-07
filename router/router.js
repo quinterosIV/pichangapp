f1bdffe1a2cdaf12ffaf9656db0b161155aa7593
Joaquin <joaquin.rodriguez@sansano.usm.cl>
2018-11-07 18:19:42 -0300

    Se actualiza el master

--- router/router.js
+++ router/router.js
@@ -3,10 +3,16 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const loginController = require('../controllers/loginController');
 const teamController = require('../controllers/teamController');
+const matchController = require('../controllers/matchController');
 
 router.route('/usuario').post(userController.createUser);
+router.route('/usuario').post(userController.getUsers);
+router.route('/usuario/add').put(userController.addFriend);
+router.route('/listFriends').get(userController.listFriends);
 router.route('/login').post(loginController.login);
 router.route('/equipo').post(teamController.createTeam);
 router.route('/calificar').put(teamController.calificar);
+router.route('/:username/crearpartido').post(matchController.createMatch);
+
 
 module.exports = router;
